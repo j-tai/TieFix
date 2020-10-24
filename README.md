23f50ddd8760bdf939268c6725e6321b49c1f83c
Jasmine Tai <jtai@jtai.ca>
2020-10-23 22:01:15 -0400

    Add workaround for MC-127970

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ A Fabric mod that fixes some annoying Minecraft bugs
 
 ## Bugs fixed
 
+* [**MC-127970**](https://bugs.mojang.com/browse/MC-127970) Using riptide on a trident with an item in your off-hand causes visual glitch with the item in your offhand
+  * Workaround: Render items in hands in regular place while using riptide.
 * [**MC-197616**](https://bugs.mojang.com/browse/MC-197616) Certain custom biome settings cause game to spam "Received invalid biome id: -1" in the console, causing major lag or freeze
+  * Workaround: Do not log these warnings
 
 ## License
 
---
+++ src/main/java/ca/jtai/tiefix/mixin/FixMC127970Mixin.java
@@ -0,0 +1,23 @@
+package ca.jtai.tiefix.mixin;
+
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * MC-127970: Using riptide on a trident with an item in your off-hand causes visual glitch with the item in your
+ * offhand
+ */
+@Mixin(HeldItemRenderer.class)
+public class FixMC127970Mixin {
+    @Redirect(
+            method = "renderFirstPersonItem",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;isUsingRiptide()Z")
+    )
+    private boolean isUsingRiptideProxy(AbstractClientPlayerEntity obj) {
+        // Assume the player is not using riptide for the purpose of rendering the first person held item
+        return false;
+    }
+}
