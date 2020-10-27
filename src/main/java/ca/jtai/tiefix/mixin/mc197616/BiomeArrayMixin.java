d1d4bbc25ca13bf5975cec26c32394cab12c4f45
Jasmine Tai <jtai@jtai.ca>
2020-10-27 13:00:16 -0400

    Reorganize mixins into packages

--- src/main/java/ca/jtai/tiefix/mixin/mc197616/BiomeArrayMixin.java
+++ src/main/java/ca/jtai/tiefix/mixin/mc197616/BiomeArrayMixin.java
@@ -1,4 +1,4 @@
-package ca.jtai.tiefix.mixin;
+package ca.jtai.tiefix.mixin.mc197616;
 
 import ca.jtai.tiefix.TieFixClient;
 import net.minecraft.world.biome.source.BiomeArray;
@@ -7,12 +7,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-/**
- * MC-197616: Certain custom biome settings cause game to spam "Received invalid biome id: -1" in the console, causing
- * major lag or freeze
- */
 @Mixin(BiomeArray.class)
-public class FixMC197616Mixin {
+public class BiomeArrayMixin {
     @Redirect(
             method = "<init>(Lnet/minecraft/util/collection/IndexedIterable;[I)V",
             at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V")
