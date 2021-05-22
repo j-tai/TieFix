75649d70e0655c70ed819550943f4aff89205a72
James Tai <jtai@jtai.ca>
2021-05-22 16:49:50 -0700

    Add fix for MC-145929

---
+++ src/main/java/ca/jtai/tiefix/mixin/mc145929/InGameHudMixin.java
@@ -0,0 +1,26 @@
+package ca.jtai.tiefix.mixin.mc145929;
+
+import ca.jtai.tiefix.TieFixClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+    @Redirect(
+        method = "render",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I")
+    )
+    private int renderProxy(TextRenderer textRenderer, MatrixStack matrices, Text text, float x, float y, int color) {
+        // Change to drawWithShadow if the fix is enabled
+        if (TieFixClient.getConfig().mc145929_fix) {
+            return textRenderer.drawWithShadow(matrices, text, x, y, color);
+        } else {
+            return textRenderer.draw(matrices, text, x, y, color);
+        }
+    }
+}
