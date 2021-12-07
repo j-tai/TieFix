3fd9111e0c33a43052225ae71f0fb1e011f26299
Jasmine Tai <jtai@jtai.ca>
2021-12-06 19:36:57 -0800

    Add fix for MC-2071

--- src/main/java/ca/jtai/tiefix/config/Config.java
+++ src/main/java/ca/jtai/tiefix/config/Config.java
@@ -1,6 +1,7 @@
 package ca.jtai.tiefix.config;
 
 public class Config {
+    public boolean mc2071_fix = true;
     public boolean mc122477_fix = true;
     public String mc122477_keys = "";
     public boolean mc127970_fix = true;
