d2671a636e229392b8caef2ee7fd5a991d29ee45
James Tai <jtai@jtai.ca>
2022-04-09 13:41:26 -0700

    Add fix for MC-12829

---
+++ src/main/java/ca/jtai/tiefix/mixin/mc12829/LivingEntityMixin.java
@@ -0,0 +1,23 @@
+package ca.jtai.tiefix.mixin.mc12829;
+
+import ca.jtai.tiefix.TieFix;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = LivingEntity.class, priority = TieFix.MIXIN_PRIORITY)
+public class LivingEntityMixin {
+    @Inject(method = "isClimbing", at = @At("HEAD"), cancellable = true)
+    private void onIsClimbing(CallbackInfoReturnable<Boolean> cir) {
+        if (!TieFix.getConfig().mc12829_fix) {
+            return;
+        }
+        //noinspection ConstantConditions
+        if ((Object) this instanceof PlayerEntity player && player.getAbilities().flying) {
+            cir.setReturnValue(false);
+        }
+    }
+}
