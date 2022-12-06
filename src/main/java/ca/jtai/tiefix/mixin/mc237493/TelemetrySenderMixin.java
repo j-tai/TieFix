deb98247b1b9073d4da1ad6391b7261d1ffe623c
Jasmine Tai <jtai@jtai.ca>
2022-12-05 22:08:05 -0800

    Prepare for 1.19.3 update

--- src/main/java/ca/jtai/tiefix/mixin/mc237493/TelemetrySenderMixin.java
+++ src/main/java/ca/jtai/tiefix/mixin/mc237493/TelemetrySenderMixin.java
@@ -1,54 +1,19 @@
 package ca.jtai.tiefix.mixin.mc237493;
 
 import ca.jtai.tiefix.TieFix;
-import com.mojang.authlib.minecraft.TelemetrySession;
-import com.mojang.authlib.minecraft.UserApiService;
-import net.minecraft.SharedConstants;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.telemetry.TelemetryManager;
 import net.minecraft.client.util.telemetry.TelemetrySender;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Optional;
-import java.util.UUID;
-
-@Mixin(value = TelemetrySender.class, priority = TieFix.MIXIN_PRIORITY)
+@Mixin(value = TelemetryManager.class, priority = TieFix.MIXIN_PRIORITY)
 public class TelemetrySenderMixin {
-    @Shadow
-    @Final
-    @Mutable
-    private TelemetrySession session;
-    @Shadow
-    private boolean sent;
-
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void onInit(
-        MinecraftClient client,
-        UserApiService userApiService,
-        Optional<String> userId,
-        Optional<String> clientId,
-        UUID deviceSessionId,
-        CallbackInfo ci
-    ) {
+    @Inject(method = "getSender", at = @At("HEAD"), cancellable = true)
+    private void onGetSender(CallbackInfoReturnable<TelemetrySender> cir) {
         if (TieFix.getConfig().mc237493_fix) {
-            session = TelemetrySession.DISABLED;
-            sent = true;
-        }
-    }
-
-    @Redirect(method = "<init>", at = @At(value = "FIELD", target = "Lnet/minecraft/SharedConstants;isDevelopment:Z"))
-    private boolean isDevelopmentProxy() {
-        if (TieFix.getConfig().mc237493_fix) {
-            // Pretend that this is a development build, which doesn't send telemetry
-            return true;
-        } else {
-            return SharedConstants.isDevelopment;
+            cir.setReturnValue(TelemetrySender.NOOP);
         }
     }
 }
