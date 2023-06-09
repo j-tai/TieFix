10811ae67d881758a417aae5c685e3d3277acc11
James Tai <jtai@jtai.ca>
2023-06-08 20:16:07 -0700

    Update sizemap to support characters outside BMP

--- src/main/java/ca/jtai/tiefix/fixes/mc89242/Sizemap.java
+++ src/main/java/ca/jtai/tiefix/fixes/mc89242/Sizemap.java
@@ -1,5 +1,8 @@
 package ca.jtai.tiefix.fixes.mc89242;
 
+import net.minecraft.text.Style;
+import net.minecraft.text.TextVisitFactory;
+
 /**
  * A mapping of characters to their widths when rendered.
  * <p>
@@ -11,22 +14,24 @@ package ca.jtai.tiefix.fixes.mc89242;
  * <em>if</em> it were rendered in the default font.
  */
 public class Sizemap {
-    private byte[] sizes = new byte[0x10000];
+    // Yeah, this is 1MB of memory... but we all have 1MB to spare, right?
+    private byte[] sizes = new byte[0x110000];
 
-    public int getWidth(char ch) {
-        return sizes[ch];
+    public int getWidth(int codepoint) {
+        return sizes[codepoint];
     }
 
     public int getWidth(String text) {
-        var width = 0;
-        for (var i = 0; i < text.length(); i++) {
-            width += getWidth(text.charAt(i));
-        }
-        return width;
+        int[] total = {0};
+        TextVisitFactory.visitForwards(text, Style.EMPTY, (index, style, codepoint) -> {
+            total[0] += getWidth(codepoint);
+            return true;
+        });
+        return total[0];
     }
 
-    public void set(char ch, int size) {
+    public void set(int codepoint, int size) {
         assert size >= 0 && size <= Byte.MAX_VALUE;
-        sizes[ch] = (byte) size;
+        sizes[codepoint] = (byte) size;
     }
 }
