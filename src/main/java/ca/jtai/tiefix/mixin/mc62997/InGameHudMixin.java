5ccf6ac5edb3653deea73b80be772c7e1ab76312
Jasmine Tai <jtai@jtai.ca>
2023-06-08 19:48:51 -0700

    Update to 1.20

--- src/main/java/ca/jtai/tiefix/mixin/mc62997/InGameHudMixin.java
+++ src/main/java/ca/jtai/tiefix/mixin/mc62997/InGameHudMixin.java
@@ -2,6 +2,7 @@ package ca.jtai.tiefix.mixin.mc62997;
 
 import ca.jtai.tiefix.TieFix;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.scoreboard.ScoreboardObjective;
@@ -13,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = InGameHud.class, priority = TieFix.MIXIN_PRIORITY)
 public class InGameHudMixin {
     @Inject(method = "renderScoreboardSidebar", at = @At("HEAD"), cancellable = true)
-    private void onRenderScoreboardSidebar(MatrixStack matrices, ScoreboardObjective objective, CallbackInfo ci) {
+    private void onRenderScoreboardSidebar(DrawContext context, ScoreboardObjective objective, CallbackInfo ci) {
         if (TieFix.getConfig().mc62997_fix && MinecraftClient.getInstance().options.debugEnabled) {
             ci.cancel();
         }
